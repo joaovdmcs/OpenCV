093db42db6ac313970a86ac196a718b54f4ddcd5
João Vitor de Melo <44266120+joaovdmcs@users.noreply.github.com>
2024-04-25 12:55:45 -0300

    Delete Primeira Etapa directory

--- Primeira Etapa/2-OpenCV/exemplo2.4.py
+++
@@ -1,9 +0,0 @@
-import cv2
-imagem = cv2.imread('entrada.jpg')
-
-for i in range(0,imagem.shape[0]):
-    for j in range(0,imagem.shape[1]):
-        imagem[i,j] = (0,(i*j)%256,0) 
-
-cv2.imshow('Imagem modificada', imagem)
-cv2.waitKey(0) 
